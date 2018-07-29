ecb5daae10c7de5c79f3f675b100c52e9dc966bb
andrew kelly <andyckelly@outlook.com>
2018-07-29 15:27:45 +0100

    xaria update

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
-var api = 'http://127.0.0.1:11898';
+var api = 'http://127.0.0.1:11998';
 var donationAddress = "";
-var blockTargetInterval = 30;
+var blockTargetInterval = 120;
 var coinUnits = 100;
-var symbol = 'trtl';
+var symbol = 'XARI';
 var refreshDelay = 30000;
 // pools stats by MainCoins
 var networkStat = {
