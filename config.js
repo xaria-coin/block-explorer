0e3917aa6fb404b4c7a6c164ebd2eccb25cfe5c5
andrew kelly <andyckelly@outlook.com>
2018-08-09 06:53:23 +0100

    updates

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
-var api = 'http://45.76.146.102:11998';
+var api = 'http://45.76.146.102:11966';
 var donationAddress = "";
 var blockTargetInterval = 120;
 var coinUnits = 100;
@@ -7,7 +7,7 @@ var refreshDelay = 30000;
 // pools stats by MainCoins
 var networkStat = {
  "XARI": [
-	["Main Pool", "http://159.69.85.179:8119"]
+	["Main Pool", "http://mine.xaria.xyz:8117"]
  ]
 };
 
