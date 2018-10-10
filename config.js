66a2261051c05f7644ec0d77236e9ead8901c836
XARIA <dev@xaria.xyz>
2018-10-10 19:31:00 +0100

    Update config.js

--- config.js
+++ config.js
@@ -2,17 +2,21 @@ var api = 'http://45.76.146.102:11966';
 var donationAddress = "";
 var blockTargetInterval = 120;
 var coinUnits = 100;
-var symbol = 'XARI';
+var symbol = 'xari';
 var refreshDelay = 30000;
 // pools stats by MainCoins
 var networkStat = {
- "XARI": [
-	["Main Pool", "http://mine.xaria.xyz:8117/stats"]
+ "xari": [
+	["pool.xaria.xyz", "http://217.69.13.137:8117"],
+	["Official Pool (old - don't use)", "http://159.69.85.179:8117"],
+	["xari.worktipspool.xyz", "http://xari.worktipspool.xyz:8117"],
+	["xaria.multiminer.us", "http://xaria.multiminer.us:8117"],
+	["xaria.yourmine.xyz", "http://xaria.yourmine.xyz:8117"]
  ]
 };
 
 var networkStat2 = {
-    "XARI": [
+    "xari": [
 	[""]
  ]
 };
