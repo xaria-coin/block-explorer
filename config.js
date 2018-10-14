ca24e7114448ab530a71f679c1e798f82ae2bc81
andrew kelly <andyckelly@outlook.com>
2018-10-14 08:58:06 +0100

    updated pools

--- config.js
+++ config.js
@@ -8,7 +8,6 @@ var refreshDelay = 30000;
 var networkStat = {
  "xari": [
 	["pool.xaria.xyz", "http://217.69.13.137:8117"],
-	["Official Pool (old - don't use)", "http://159.69.85.179:8117"],
 	["xari.worktipspool.xyz", "http://xari.worktipspool.xyz:8117"],
 	["xaria.multiminer.us", "http://xaria.multiminer.us:8117"],
 	["xaria.yourmine.xyz", "http://xaria.yourmine.xyz:8117"]
